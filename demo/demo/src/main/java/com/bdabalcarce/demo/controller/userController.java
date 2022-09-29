80a3e20bb4b483acfcde82d4ac6e6e4dd8ec769a
germanpistan <anibalgermanpistan@gmail.com>
2022-09-29 13:14:06 -0300

    funcionando jueves2

--- demo/demo/src/main/java/com/bdabalcarce/demo/controller/userController.java
+++ demo/demo/src/main/java/com/bdabalcarce/demo/controller/userController.java
@@ -1,14 +1,14 @@
 package com.bdabalcarce.demo.controller;
 
 
+import com.bdabalcarce.demo.Dto.userDto;
 import com.bdabalcarce.demo.entity.user;
+import com.mysql.cj.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.bdabalcarce.demo.service.userS;
 
 import java.util.List;
@@ -25,6 +25,24 @@ public class userController {
     public ResponseEntity<List<user>> list() {
         List<user> list = userServ.list();
         return new ResponseEntity(list, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody userDto dtopersona) {
+        if (StringUtils.isNullOrEmpty(dtopersona.getUserName())) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        user User = new user(dtopersona.getUserName(),
+                dtopersona.getUserLastname(),
+                dtopersona.getUserEmail(),
+                dtopersona.getUserPhone(),
+                dtopersona.getUserDireccion(),
+                dtopersona.getUserVehicle(),
+                dtopersona.getUserDiponibilidad());
+        userServ.save(User);
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
